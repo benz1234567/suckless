25cb225ba7a4dff3f3db1abe06936ae8ecc543af
Ben Z <benzgherea@gmail.com>
2024-04-11 20:23:53 -0700

    Changed to Nerd font, statusallmons

--- st/config.h
+++ st/config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Cascadia Code:pixelsize=15:antialias=true:autohint=true";
+//static char *font = "Cascadia Code:pixelsize=15:antialias=true:autohint=true";
+static char *font = "CaskaydiaCove Nerd Font,CaskaydiaCove:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
