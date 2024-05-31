ffde2d8b7badea3b7f37927192e5aa7413b43d6d
Ben Z <benzgherea@gmail.com>
2024-05-31 12:58:17 -0700

    idk

--- .scripts/nightlight.sh
+++ .scripts/nightlight.sh
@@ -2,8 +2,10 @@ while true;
 do
 	if [ $(date '+%H') -gt "18" ] || [ $(date '+%H') -lt "6" ]; then
 		redshift -O 1000
+		feh --bg-fill ~/.backgrounds/blankwhite.png
 	else
 		redshift -x
+		feh --bg-fill ~/.backgrounds/wallhaven-p9vjq9_1920x1080.png
 	fi
 	sleep 1
 done
