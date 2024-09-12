0a41a02bd9ebe4e51b5d8815aad13e31487da01d
Ben Z <benzgherea@gmail.com>
2024-09-12 15:33:48 -0700

    gitpushcommand 09-12-24

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -19,8 +19,8 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_black, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_black, col_black },
+	[SchemeSel]  = { col_gray4, col_gray2,  col_gray2  },
 };
 
 /* tagging */
